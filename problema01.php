6486c8438d4d715137f8e013167f8c52a679c5aa
Porcino <jorgelvr99@gmail.com>
2021-11-01 09:26:53 -0500

    Version 02

---
+++ problema01.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt mollitia voluptates reprehenderit, perferendis debitis hic? Itaque sit tempora asperiores ad architecto quo accusantium a adipisci reiciendis doloribus voluptatem, sint natus.
+
+    </p>
+</body>
+</html>
